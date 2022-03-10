1c35fc1fa96561a415b32d1500c5bbfc46e93a70
Timothy Gaither <timjgaither@gmail.com>
2022-03-10 18:13:22 +0100

    push

--- Snack-5/index.php
+++ Snack-5/index.php
@@ -10,23 +10,10 @@ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
- $str=explode(".",$testo);
+$str=explode(".",$testo);
 
+
+for($i=0; $i < count($str);$i++){
+   echo $str[$i] . '<br>';
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<p><?php echo ($str[0])?></p>
-<p><?php echo ($str[1])?></p>
-<p><?php echo ($str[2])?></p>
-<p><?php echo ($str[3])?></p>
-    
-</body>
-</html>
