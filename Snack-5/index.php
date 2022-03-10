a0dd7dca35565e03345181bc2c37f8af6422902a
Timothy Gaither <timjgaither@gmail.com>
2022-03-10 17:25:34 +0100

    push

--- Snack-5/index.php
+++ Snack-5/index.php
@@ -5,12 +5,13 @@ Prendere il testo e suddividerlo in tanti paragrafi ogni volta
 in cui si incontrerà un punto. -->
 
 <?php
-$testo="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\r\n 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\r\n
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \r\n
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r";
+$testo="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-$testo_a_capo=nl2br($testo);
+explode(". ",$testo);
+$stringa=var_dump($testo)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,11 +23,7 @@ $testo_a_capo=nl2br($testo);
 </head>
 <body>
 
-<h1>Testo con a capo automatici=</h1>
-<p><?php echo $testo?></p>
-
-<h1>Testo con a capo manuali=</h1>
-<p><?php echo $testo_a_capo?></p>
+<p><?php echo $stringa?></p>
     
 </body>
 </html>
